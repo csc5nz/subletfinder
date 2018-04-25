d936980517d710f19c9afcab3c643ab573dd1327
csc5nz <csc5nz@virginia.edu>
2018-04-24 20:08:10 -0400

    Quick fix import packages

--- app/src/main/java/example/com/subletfinder/AddImageActivity.java
+++ app/src/main/java/example/com/subletfinder/AddImageActivity.java
@@ -29,8 +29,14 @@ import java.util.UUID;
 
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 
 /**
